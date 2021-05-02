410d2cd63cec896108abccf5294f308775f3c41a
Yamac Ozdil <yamac.ozdil@outlook.com>
2021-05-02 13:55:39 -0400

    set up marked.js

--- src/App.js
+++ src/App.js
@@ -6,23 +6,35 @@ import {
   Divider,
   Header,
   Icon,
+  Segment,
 } from "semantic-ui-react";
+
+import marked from "marked";
+
+const message = "";
+
+function createMarkup() {
+  let rawMarkup = marked("# HERE", { sanitize: true });
+  return { __html: rawMarkup };
+}
 
 function App() {
   return (
     <Container className="App">
       <Form>
         <Header as="h2" image={<Icon name="write" />} content="Editor" />
-        <TextArea id="editor" rows={5} placeholder="Tell us more" />
+        <TextArea
+          id="editor"
+          rows={5}
+          value="The value from here should go into #HERE!"
+        />
       </Form>
       <Divider />
       <Form>
         <Header as="h2" image={<Icon name="eye" />} content="Preview" />
-        <TextArea
-          id="preview"
-          rows={5}
-          value="User Story #5: When my markdown previewer first loads, the default text in the #editor field should contain valid markdown that represents at least one of each of the following elements: a header (H1 size), a sub header (H2 size), a link, inline code, a code block, a list item, a blockquote, an image, and bolded text."
-        />
+        <Segment>
+          <div id="preview" dangerouslySetInnerHTML={createMarkup()} />
+        </Segment>
       </Form>
     </Container>
   );
