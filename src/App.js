1df41eafd5aad750cf7629c32846ce8945a1283d
Yamac Ozdil <yamac.ozdil@outlook.com>
2021-05-02 12:40:28 -0400

    add semantic UI

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import "./styles/app.scss";
+import { Button } from "semantic-ui-react";
 
 function App() {
   return (
     <div className="App">
       <h1>Hello</h1>
+      <Button>Click Here</Button>
     </div>
   );
 }
